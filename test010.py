80d84a65d3bf27ed7fbe8a5f8a01655158f42636
SeoHyun <dnltjguss@gmail.com>
2024-07-29 18:18:01 +0900

    dfdf

--- test010.py
+++ test010.py
@@ -1,3 +1,9 @@
 print("gdgdgdg")
 #행운의 편지
 print("dfdfd1111")
+print("ddkssudgktpdy")
+print("ddkssudgktpdy")
+print("ddkssudgktpdy")
+print("ddkssudgktpdy")
+print("ddkssudgktpdy")
+print("ddkssudgktpdy")
